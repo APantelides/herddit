7ca4a30b46b1167ab6b900a5409e7933fab05cf7
Alex Pantelides <alex.pantelides@gmail.com>
2016-08-29 16:58:39 -0700

    Define the song model

---
+++ client/models/song.js
@@ -0,0 +1,11 @@
+var app = app || {};
+
+app.Song = Backbone.Model.extend({
+  defaults: {
+    title: 'No Title',
+    artist: 'Unknown',
+    genre: 'Not Specified',
+    url: 'http://www.example.com/'
+  }
+
+});
