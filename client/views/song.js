8c31b605e21fbd562f67dcaaddea7049d95632a2
Alex Pantelides <alex.pantelides@gmail.com>
2016-08-29 21:15:53 -0700

    Implement upvoting and connect to the database

--- client/views/song.js
+++ client/views/song.js
@@ -5,9 +5,18 @@ app.SongView = Backbone.View.extend({
   className: 'songContainer',
   template: _.template( $( '#songs-template' ).html() ),
 
+  events: {
+    'click .upvote': 'upVote'
+  },
+
   render: function() {
     this.$el.html( this.template( this.model.attributes ) );
 
     return this;
+  },
+
+  upVote: function () {
+    this.model.set('upvotes', this.model.get('upvotes') + 1 );
+    this.render();
   }
 });
--- server/app.js
+++ server/app.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var Sequelize = require('sequelize');
+var bodyParser = require('body-parser');
 var port = 3000;
 
+var sequelize = new Sequelize('herddit', 'apantelides', null, {
+  dialect: 'postgres',
+  port: 5432
+});
+
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  }, function (err) { 
+    console.log('Unable to connect to the database:', err);
+  });
 
 app.use(express.static(path.join(__dirname, '../client')));
+
+app.use(bodyParser());
 
 app.listen(port, function() {
   console.log('Server started! Listening on port:' + port);
