c63081deabfe1020ea9d6946eec0dba17bf39778
Alex Pantelides <alex.pantelides@gmail.com>
2016-08-29 19:54:27 -0700

    Implement adding of new links

--- client/views/list.js
+++ client/views/list.js
@@ -3,9 +3,14 @@ var app = app || {};
 app.ListView = Backbone.View.extend({
   el: '#songs',
 
+  events: {
+    'click #submitSong': 'addSong'
+  },
+
   initialize: function ( initialSongList ) {
     this.collection = new app.List( initialSongList );
     this.render();
+    this.listenTo( this.collection, 'add', this.renderSong );
   },
 
   render: function () {
@@ -19,5 +24,17 @@ app.ListView = Backbone.View.extend({
       model: song
     });
     this.$el.append( songView.render().el );
+  },
+  addSong: function ( e ) {
+    e.preventDefault();
+
+    var formData = {};
+
+    $( '#addSong div').children( 'input' ).each(function (i, el) {
+      if ( $(el).val() !== '') {
+        formData[ el.id ] = $(el).val();
+      }
+    });
+    this.collection.add(new app.Song( formData ));
   }
 });
